33ab146e69ea40cafc6f9c48d55b8a5bdd5387cd
aasdkl <hsuehchiahao@gmail.com>
2016-11-10 18:25:59 +0800

    comm

--- src/main/java/rms/config/WebConfig.java
+++ src/main/java/rms/config/WebConfig.java
@@ -1,7 +1,5 @@
 package rms.config;
 
-import java.io.File;
-
 import rms.model._MappingKit;
 
 import com.jfinal.config.Constants;
@@ -11,10 +9,11 @@ import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.json.FastJsonFactory;
-import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
 import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.druid.DruidPlugin;
+
 import rms.controller.MainController;
 
 public class WebConfig extends JFinalConfig{
@@ -33,7 +32,7 @@ public class WebConfig extends JFinalConfig{
 	
 	@Override
 	public void configPlugin(Plugins plugins) {
-		C3p0Plugin cp = new C3p0Plugin("jdbc:mysql://localhost:3306/risk?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull", "root", "");
+		DruidPlugin cp = new DruidPlugin("jdbc:mysql://localhost:3306/risk?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull", "root", "");
 		plugins.add(cp);
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(cp);
 		arp.setDialect(new MysqlDialect());
